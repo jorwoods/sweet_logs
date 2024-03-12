93c22d54cfdfb2380bcc9ed561bbd8de613a4724
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-03-11 21:24:04 -0500

    fix: remove comment

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -18,8 +18,6 @@ def get_date(monkeypatch):
 def test_JSONFormatter_no_fmt_keys(get_date):
     fmt = sweet_logs.formatters.JSONFormatter()
 
-    # def __init__(self, name, level, pathname, lineno,
-    #              msg, args, exc_info, func=None, sinfo=None, **kwargs):
     record = logging.LogRecord(name="test", level=10, pathname="pytest", lineno=10, exc_info=None, msg="test", args=())
     output = fmt.format(record)
     record_dict = json.loads(output)
