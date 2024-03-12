cd2a9af43584c74960b7235a39c7c100b4306fc1
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-03-11 20:39:43 -0500

    test: monkeypatch timestamp creation

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -2,12 +2,20 @@ import datetime as dt
 import logging
 import json
 
-# import pytest
+import pytest
 
 import sweet_logs
 
 
-def test_JSONFormatter_no_fmt_keys():
+@pytest.fixture
+def get_date(monkeypatch):
+    def mock_date(*args, **kwargs):
+        return dt.datetime(2024, 1, 1, 0, 0, 0, 0, tzinfo=dt.timezone.utc)
+
+    monkeypatch.setattr(sweet_logs.formatters, "_date_from_timestamp", mock_date)
+
+
+def test_JSONFormatter_no_fmt_keys(get_date):
     fmt = sweet_logs.formatters.JSONFormatter()
 
     # def __init__(self, name, level, pathname, lineno,
@@ -15,6 +23,7 @@ def test_JSONFormatter_no_fmt_keys():
     record = logging.LogRecord(name="test", level=10, pathname="pytest", lineno=10, exc_info=None, msg="test", args=())
     output = fmt.format(record)
     record_dict = json.loads(output)
-    dt.datetime.fromisoformat(record_dict["timestamp"])
+    parsed_date = dt.datetime.fromisoformat(record_dict["timestamp"])
 
     assert record_dict["message"] == "test"
+    assert parsed_date == dt.datetime(2024, 1, 1, 0, 0, 0, 0, tzinfo=dt.timezone.utc)
