9c4cfb79f2498a163bbc0f08ca86edff713e6bf9
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-03-25 22:27:14 -0500

    feat: add function for setting up logging

--- src/sweet_logs/__init__.py
+++ src/sweet_logs/__init__.py
@@ -4,4 +4,22 @@ __author__ = """Jordan Woods"""
 __email__ = "jorwoods@example.com"
 __version__ = "0.1.0"
 
-from sweet_logs import formatters  # noqa: F401
+import logging.config
+from typing import Optional
+from sweet_logs import config, formatters  # noqa: F401
+
+
+def setup_logging(config: Optional[config.LogConfig] = None):
+    """
+    Set up logging configuration. If no configuration is provided, the default
+    configuration is used. The default configuration is defined in
+    sweet_logs.config.base_config.
+
+    Parameters
+    ----------
+    config : Optional[config.LogConfig], optional
+        A dictionary that contains configuration for the logger.
+    """
+    if config is None:
+        config = config.base_config
+    logging.config.dictConfig(config)
