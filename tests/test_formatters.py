ca1b5d65d7bdc2c28248e0f5d98113e922d08b8e
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-06-08 11:42:43 -0500

    fix: handle unknown log fmt_keys

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import logging
 import json
+import sys
 from typing import Annotated, Any, Mapping, Tuple, no_type_check
 
 import pytest
@@ -27,3 +28,37 @@ def test_JSONFormatter_no_fmt_keys(get_date: Annotated[str, pytest.fixture]) -> 
 
     assert record_dict["message"] == "test"
     assert parsed_date == dt.datetime(2024, 1, 1, 0, 0, 0, 0, tzinfo=dt.timezone.utc)
+
+
+def test_json_formatter(capfd):  # type: ignore
+    # Arrange
+    logger = logging.getLogger("test_logger")
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(stream=sys.stdout)
+    ch.setLevel(logging.DEBUG)
+
+    formatter = sweet_logs.formatters.JSONFormatter(
+        fmt_keys={
+            "level": "levelname",
+            "message": "message",
+        }
+    )
+    ch.setFormatter(formatter)
+
+    logger.addHandler(ch)
+
+    # Act
+    logger.debug("debug message")
+
+    # Assert
+    cap = capfd.readouterr()
+    log_output = cap.out
+    log_dict = json.loads(log_output)
+
+    assert log_dict["level"] == "DEBUG"
+    assert log_dict["message"] == "debug message"
+    assert "timestamp" in log_dict
+
+    date = dt.datetime.fromisoformat(log_dict["timestamp"])
+    assert isinstance(date, dt.datetime)
